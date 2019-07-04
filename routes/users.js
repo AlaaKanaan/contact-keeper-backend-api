1f58b900ac6b367463e305a01a9bb2e8ea1c6340
akanaan278@gmail.com <Vodocoolfr278>
2019-07-04 18:13:49 +0300

    update

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const {check, validationResult} = require('express-validator');
 
 // @route   POST api/users
@@ -46,7 +48,21 @@ router.post('/', [
 
         await user.save();
 
-        res.send("User Saved");
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn: 360000},
+            (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            }
+        );
 
     } catch (err) {
         console.error(err.message);
